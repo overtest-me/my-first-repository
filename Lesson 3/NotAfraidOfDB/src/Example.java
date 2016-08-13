b867b9946405ea8952b95dc18b346fa09ec260b9
Overtest.me <test@overtest.me>
2016-08-13 22:11:21 +0300

    Added project for DB example

---
+++ Lesson 3/NotAfraidOfDB/src/Example.java
@@ -0,0 +1,43 @@
+import java.sql.*;
+
+public class Example {
+
+    // 01. Define constants for the database URL and user's credentials
+    private static final String DATABASE_URL = "jdbc:mysql://localhost/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static void main(String[] args) {
+
+        // 02. Open a connection to the local DB with provided credentials
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+             Statement statement = connection.createStatement();) {
+
+            /* 03. Defining query string for obtaining all data from our DB
+            executing it and saving the result in the ResultSet object */
+            String myQuery = "SELECT * FROM test.company";
+            ResultSet result = statement.executeQuery(myQuery);
+
+            // 04. Iterating through all DB rows we have in the ResultSet object
+            while (result.next()) {
+
+                /* 05. Obtaining value of each column for a single row:
+                ID, name, number of employees, is company profitable*/
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                int numOfEmpl = result.getInt("numOfEmployee");
+                boolean isProfitable = result.getBoolean("isProfitable");
+
+                // Output to the console to make sure that we got some data
+                System.out.println(String.format("ID: %s, Name: %s, Number of employees: %s, Is profitable: %s",
+                        id, name, numOfEmpl, isProfitable));
+            }
+
+            // 05. Closing the ResultSet object
+            result.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
